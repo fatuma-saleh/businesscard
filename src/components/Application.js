fe4a05031e9cc375f9396670cacc47fa458fd516
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-03 13:46:06 -0500

    Add SavedCards to App

--- src/components/Application.js
+++ src/components/Application.js
@@ -16,6 +16,7 @@ export default function Application(props) {
       <section>
         {page === "MainPage" && <MainPage />}
         {page === "MyCards" && <MyCards />}
+        {page === "SavedCards" && <SavedCards />}
       </section>
     </main>
   )
