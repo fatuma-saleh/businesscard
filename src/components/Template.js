dcd6bfbac794df2454dac2937e71f49b106c40aa
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-03 19:09:59 -0500

    Insert boilerplate component

---
+++ src/components/Template.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+export default function Template(props) {
+  return (
+    <Fragment>
+      Template
+      <p>name</p>
+      <p>title</p>
+      <p>email</p>
+      <p>phone</p>
+      <p>media</p>
+      <p>company</p>
+    </Fragment>
+  )
+}
