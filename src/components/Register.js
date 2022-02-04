839431adca6b61e493440ace3cf0f0677f823c0a
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-03 19:12:46 -0500

    Insert boilerplate component

---
+++ src/components/Register.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+export default function Register(props) {
+  return (
+    <Fragment>
+      Register
+      <p>name</p>
+      <p>email</p>
+    </Fragment>
+  )
+}
