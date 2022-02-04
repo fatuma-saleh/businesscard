be74951cb1e00ce342c629ed67f98a9374faab24
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-03 19:05:44 -0500

    Put in component with placeholder text

---
+++ src/components/NewCards.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+export default function NewCards(props) {
+  return (
+    <Fragment>
+      New Cards
+      <p>name</p>
+      <p>title</p>
+      <p>email</p>
+      <p>phone</p>
+      <p>media</p>
+      <p>company</p>
+    </Fragment>
+  )
+}
