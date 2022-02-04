90677bc329823660b7a02177c414b96dc2d04d4f
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-03 19:13:35 -0500

    Add Register component to App

--- src/components/Application.js
+++ src/components/Application.js
@@ -23,6 +23,7 @@ export default function Application(props) {
         {page === "SavedCards" && <SavedCards />}
         {page === "NewCards" && <NewCards />}
         {page === "Template" && <Template />}
+        {page === "Register" && <Register />}
       </section>
     </main>
   )
