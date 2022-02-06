1f7bc71ee12d35c366657b50dd1c7c6e381d07fb
yuhaoyann <yuhaoyann@gmail.com>
2022-02-06 01:28:02 -0500

    send axios.get to API and render mycards using return data

--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -3,55 +3,56 @@ import { Fragment } from "react";
 import "./MyCards.scss"
 
 // ** For building card page
-const card = {
-  first_name: "haoyan",
-  last_name: "yu",
-  photo: "https://i.imgur.com/Nmx0Qxo.png",
-  title: "student",
-  organization: "lighthouse-labs",
-  phone: "780-655-7545",
-  email: "yuhaoyann@gmail.com",
-  github: "github",
-  linkedin: "linkedin",
-  instagram: "instagram"
-}
+// const card = {
+//   first_name: "haoyan",
+//   last_name: "yu",
+//   fullname: "haoyan yu",
+//   photo: "https://i.imgur.com/Nmx0Qxo.png",
+//   title: "student",
+//   organization: "lighthouse-labs",
+//   phone: "780-655-7545",
+//   email: "yuhaoyann@gmail.com",
+//   github: "github",
+//   linkedin: "linkedin",
+//   instagram: "instagram"
+// }
 
 export default function MyCards(props) {
   return (
     <Fragment>
-      <div class="card">
-        <div class="card_photo">
+      <div className="card">
+        <div className="card_photo">
           <img
-            src={card.photo}
+            src={props.card[0].photo}
             alt="amator"
           />
         </div>
-        <div class="card_body">
-          <div class="card_content">
-            <div class="card_heading">
-              <h5 class="card_name">{`${card.first_name} ${card.last_name}`}</h5>
-              <p class="card_title">{card.title}</p>
-              <p class="card_organization">{card.organization}</p>
+        <div className="card_body">
+          <div className="card_content">
+            <div className="card_heading">
+              <h5 className="card_name">{`${props.card[0].fullname}`}</h5>
+              <p className="card_title">{props.card[0].title}</p>
+              <p className="card_organization">{props.card[0].organization}</p>
             </div>
-            <ul class="card_list">
+            <ul className="card_list">
               {/* substitute with real link later */}
-              <li class="card_list-itme">
-                <a href="phone_link">{card.phone}</a>
+              <li className="card_list-itme">
+                <a href="phone_link">{props.card[0].phone}</a>
               </li>
-              <li class="card_list-itme">
-                <a href="email_link">{card.email}</a>
+              <li className="card_list-itme">
+                <a href="email_link">{props.card[0].email}</a>
               </li>
-              <li class="card_list-itme">
-                <a href="github_link">{card.github}</a>
+              <li className="card_list-itme">
+                <a href="github_link">{props.card[0].github}</a>
               </li>
-              <li class="card_list-itme">
-                <a href="linkedin_link">{card.linkedin}</a>
+              <li className="card_list-itme">
+                <a href="linkedin_link">{props.card[0].linkedin}</a>
               </li>
-              <li class="card_list-itme">
-                <a href="instagram_link">{card.instagram}</a>
+              <li className="card_list-itme">
+                <a href="instagram_link">{props.card[0].instagram}</a>
               </li>
             </ul>
-            <div class="card_options">
+            <div className="card_options">
               <a href="display">display QR code</a>
               <br />
               <a href="save">save card</a>
