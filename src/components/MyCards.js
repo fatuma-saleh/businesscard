356c5b2dec819983192d7927b11122cb58bef3c2
Fatuma <sfatex@gmail.com>
2022-02-09 17:12:42 -0500

    Comment out test code

--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Card from "./Card";
 
 export default function MyCards(props) {
+
+  // useEffect(() => {
+  //  alert("hi")
+  // }, [])
+
   const myCards = props.myCards.map(card => {
     return (
       <Card
