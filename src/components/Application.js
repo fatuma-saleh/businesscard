4933c9c1289aec12d302286a5fed84b8d854f746
Fatuma <sfatex@gmail.com>
2022-02-09 17:14:42 -0500

    Add function to get the user stored in the local storage

--- src/components/Application.js
+++ src/components/Application.js
@@ -9,36 +9,63 @@ import Login from "./Login";
 import Register from "./Register";
 
 export default function Application(props) {
-  
-  const [page, setPage] = useState("MyCards")
-  
+  const [page, setPage] = useState("MyCards");
+
   const [myCards, setMyCards] = useState([]);
   const [savedCards, setSavedCards] = useState([]);
+
+  const getHeader = function () {
+    let user = localStorage.getItem('user');
+    // console.log("*****", user)
+    if (!user){
+      return null
+    }
+    user = JSON.parse(user)
+    
+    const config = {
+      headers: { Authorization: `Bearer ${user.token}` },
+    };
+    // console.log("*****000000config", config)
+    return config
+  };
+
   useEffect(() => {
-    Promise.all([
-      axios.get("/api/mycards/1"),
-      axios.get("/api/savedcards/1")
-    ]).then((all) => {
-      setMyCards(all[0].data);
-      setSavedCards(all[1].data);
+    const headers = getHeader()
+    if (!headers){
+      return
+    }
+    console.log("herader__", headers);
+    axios.get("http://localhost:8001/api/mycards", headers)
+    .then(result => {
+      console.log("allll++", result);
+      setMyCards(result.data);
     })
-  }, [])
-  console.log(myCards)
+    
+    // Promise.all([
+    //   axios.get("http://localhost:8001/api/mycards", headers),
+    //  axios.get("/api/savedcards", headers)
+    // ]).then((all) => {
+    //   console.log("allll++", all);
+    //   setMyCards(all[0].data);
+    //   setSavedCards(all[1].data);
+    // });
+  }, []);
+  
 
   return (
     <main>
       <nav>
-        <NavTop
-          onClick = {setPage}
-        />
+        <NavTop onClick={setPage} />
       </nav>
       <section>
         {page === "MainPage" && <MainPage />}
         {page === "MyCards" && myCards[0] && <MyCards myCards={myCards} />}
-        {page === "SavedCards" && savedCards[0] && <SavedCards savedCards={savedCards} />}
-        {page === "Login" && <Login/>}
-        {page === "Register" && <Register/>}
+        {page === "SavedCards" && savedCards[0] && (
+          <SavedCards savedCards={savedCards} />
+        )}
+        {page === "Login" && <Login />}
+        {page === "Register" && <Register />}
       </section>
     </main>
-  )
+  );
 }
