818b54d821b743a72053b0c570ca76cd76ecf1d0
Fatuma <sfatex@gmail.com>
2022-02-09 20:24:48 -0500

    Add useffect to check if user is login in and render components conditionaly

--- src/components/NavTop.js
+++ src/components/NavTop.js
@@ -1,15 +1,34 @@
 import { Fragment } from "react";
-
+import { useEffect, useState } from "react";
 export default function NavTop(props) {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    let user = localStorage.getItem("user");
+    setCurrentUser(user);
+  }, []);
   return (
     <Fragment>
-      <button onClick={() => props.onClick("MainPage")}>Main Page</button>
-      <button onClick={() => props.onClick("MyCards")}>My Cards</button>
-      <button onClick={() => props.onClick("SavedCards")}>Saved Cards</button>
-      <button onClick={() => props.onClick("")}>New Cards</button>
-      <button onClick={() => props.onClick("")}>Template</button>
-      <button onClick={() => props.onClick("Register")}>Register</button>
-      <button onClick={() => props.onClick("Login")}>Login</button>
+      {currentUser !== null ? (
+        <>
+          {" "}
+          <button onClick={() => props.onClick("MyCards")}>My Cards</button>
+          <button onClick={() => props.onClick("SavedCards")}>
+            Saved Cards
+          </button>
+          <button onClick={() => props.onClick("")}>New Cards</button>
+          <button onClick={() => props.onClick("")}>Template</button>
+          <button onClick={() => props.onClick("Logout")}>Logout</button>
+        </>
+      ) : (
+        <>
+          {" "}
+          <button onClick={() => props.onClick("MainPage")}>Main Page</button>
+          <button onClick={() => props.onClick("Register")}>Register</button>
+          <button onClick={() => props.onClick("Login")}>Login</button>
+        </>
+      )}
     </Fragment>
-  )
+  );
 }
+
+  
