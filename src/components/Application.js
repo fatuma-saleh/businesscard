d9c2163730f35761c1f3d5d6e3de6c01a9d858d4
Stéphane Krims <81576974+stephkri@users.noreply.github.com>
2022-02-10 16:44:56 -0500

    Revert "Feature/user registeration"

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,80 +5,36 @@ import NavTop from "./NavTop";
 import MainPage from "./MainPage";
 import MyCards from "./MyCards";
 import SavedCards from "./SavedCards";
-import Login from "./Login";
-import Register from "./Register";
-
 
 export default function Application(props) {
-  const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('user')));
-  const [page, setPage] = useState("MyCards");
-
+  
+  const [page, setPage] = useState("MyCards")
+  
   const [myCards, setMyCards] = useState([]);
   const [savedCards, setSavedCards] = useState([]);
-  
-
-  // const getHeader = function () {
-  //   // let user = localStorage.getItem('user');
-    
-  //   // console.log("*****", user)
-    
-    
-  //     // setPage("")
-  //     // setPage("Login")
-    
-  //   // user = JSON.parse(user)
-    
-  //   const config = {
-  //     headers: { Authorization: `Bearer ${currentUser.token}` },
-  //   };
-  //   // console.log("*****000000config", config)
-  //   return config
-  // };
-
   useEffect(() => {
-    if (!currentUser){
-      setMyCards([])
-      setSavedCards([])
-      return
-    }
-    const headers = {
-      headers: { Authorization: `Bearer ${currentUser.token}` },
-    };
-
-    // console.log("herader__", headers);
-    // axios.get("http://localhost:8001/api/mycards", headers)
-    // .then(result => {
-    //   console.log("allll++", result);
-    //   setMyCards(result.data);
-    // })
-    
     Promise.all([
-      axios.get("/api/mycards", headers),
-     axios.get("/api/savedcards", headers)
+      axios.get("/api/mycards/1"),
+      axios.get("/api/savedcards/1")
     ]).then((all) => {
-      //console.log("allll++", all);
       setMyCards(all[0].data);
       setSavedCards(all[1].data);
-    });
-  }, [currentUser]);
-  
+    })
+  }, [])
+  console.log(myCards)
 
   return (
     <main>
       <nav>
-        <NavTop onClick={setPage} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+        <NavTop
+          onClick = {setPage}
+        />
       </nav>
       <section>
         {page === "MainPage" && <MainPage />}
         {page === "MyCards" && myCards[0] && <MyCards myCards={myCards} />}
-        {page === "SavedCards" && savedCards[0] && (
-          <SavedCards savedCards={savedCards} />
-        )}
-        {page === "Login" && <Login setCurrentUser={setCurrentUser} setPage={setPage}/>}
-        {page === "Register" && <Register setPage={setPage} setCurrentUser={setCurrentUser} />}
-        
-    
+        {page === "SavedCards" && savedCards[0] && <SavedCards savedCards={savedCards} />}
       </section>
     </main>
-  );
+  )
 }
--- src/components/MyCards.js
+++ src/components/MyCards.js
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
 import Card from "./Card";
 
 export default function MyCards(props) {
-
-  // useEffect(() => {
-  //  alert("hi")
-  // }, [])
-
   const myCards = props.myCards.map(card => {
     return (
       <Card
@@ -16,10 +10,8 @@ export default function MyCards(props) {
     )
   })
   return (
-    <>
-    <h2>Mycards</h2>
     <ul>
       {myCards}
-    </ul></>
+    </ul>
   )
 }
