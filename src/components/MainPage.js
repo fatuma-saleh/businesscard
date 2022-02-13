c4a81127aeeb9c4187bb3444523a9414e289cf49
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-13 15:09:21 -0500

    Add folder for logo image and import to main page
    Former-commit-id: d35b646fedd2ae5ccbe00d203ba4583cbf68af63

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,7 +1,10 @@
+import logo from "../images/icard-logo-v1.jpg";
+
 export default function MainPage(props) {
   return (
     <section className="main-page">
     <p>This is the main page of the app</p>
+    <img className="main-logo" src={logo}></img>
     </section>
   )
 }
