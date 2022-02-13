b9e4fde8a8c3fd51671fe87a7aed03571ba81df5
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-13 15:10:36 -0500

    Stick logo to 100% width, take out placeholder text
    Former-commit-id: 09e2fbe342a4ab29158a7278f5f98d59da3d4f8f

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,7 +3,6 @@ import logo from "../images/icard-logo-v1.jpg";
 export default function MainPage(props) {
   return (
     <section className="main-page">
-    <p>This is the main page of the app</p>
     <img className="main-logo" src={logo}></img>
     </section>
   )
