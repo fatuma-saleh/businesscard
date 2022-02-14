42fdc52e85c9ed6745f11194f83eda82a5d73247
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-14 00:13:36 -0500

    Reformat SavedCards component, add text to it

--- src/components/SavedCards.js
+++ src/components/SavedCards.js
@@ -10,8 +10,12 @@ export default function SavedCards(props) {
     )
   })
   return (
+    <>
+    <h2 className="savedcards-heading">Saved Cards</h2>
+    <p>Here is where all of your friends' contact information will be stored.</p>
     <ul>
       {savedCards}
     </ul>
+    </>
   )
 }
