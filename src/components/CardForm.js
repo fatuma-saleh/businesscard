372f5d6e6b8303ec998509e8955ba7ed7772ff29
Stéphane Krims <stephane.krims@hotmail.ca>
2022-02-15 22:17:30 -0500

    Wrap form in a table, seems to be working

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -3,106 +3,111 @@ export default function CardForm(props) {
     <form onSubmit={props.handleSubmit} className="card-form">
       {props.onCancel && <p>Editing Card</p>}
       {!props.onCancel && <p>Creating New Card</p>}
-      <label>
-        Name:
-        <input
-          name="fullname"
-          type="text"
-          required="required"
-          onChange={props.handleChange}
-        />
+      <table>
+        <tr>
+          <td>Name</td>
+          <td>
+            <label>
+              <input
+                name="fullname"
+                type="text"
+                required="required"
+                onChange={props.handleChange}
+              />
+            </label>
+          </td>
+        </tr>
+        <label>
+          email:
+          <input
+            name="email"
+            type="email"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Photo:
+          <input
+            name="photo"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          title:
+          <input
+            name="title"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Company:
+          <input
+            name="company"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input
+            name="phone"
+            type="tel"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Github:
+          <input
+            name="github"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          LinkedIn:
+          <input
+            name="linkedin"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Facebook:
+          <input
+            name="facebook"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Instgram:
+          <input
+            name="instagram"
+            type="text"
+            onChange={props.handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          bio:
+          <input
+            name="bio"
+            type="text"
+            onChange={props.handleChange}
+          />
+        <br />
       </label>
-      <br />
-      <label>
-        email:
-        <input
-          name="email"
-          type="email"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Photo:
-        <input
-          name="photo"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        title:
-        <input
-          name="title"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Company:
-        <input
-          name="company"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Phone:
-        <input
-          name="phone"
-          type="tel"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Github:
-        <input
-          name="github"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        LinkedIn:
-        <input
-          name="linkedin"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Facebook:
-        <input
-          name="facebook"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Instgram:
-        <input
-          name="instagram"
-          type="text"
-          onChange={props.handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        bio:
-        <input
-          name="bio"
-          type="text"
-          onChange={props.handleChange}
-        />
-      <br />
-      </label>
+      </table>
         <input type="submit" value="Submit" />
       <br />
         {props.onCancel &&
