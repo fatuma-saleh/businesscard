36bfbaa218d8f6be08610525cb3cc09e0ae3ef28
Fatuma <sfatex@gmail.com>
2022-02-16 15:24:29 -0500

    set route to shoe QR element

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams} from "react-router-dom";
 
 import Application from "./components/Application";
 import CardShow from "./components/pages/CardShow";
 import Layout from "./components/pages/Layout";
 
 import './App.css'
+import QR from "./components/QR";
+
 
 function App() {
-
+  let { id } = useParams();
+  console.log("iiiiii",id)
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,7 +21,10 @@ function App() {
               element={<Application />} />
             <Route
               path={`showcard/:id`}
-              element={<CardShow />}
+              // element={<CardShow />}
+              // element={<QRCode value={`showcard/${props.card.id}`} size={128} />}
+              // element={<QRCode value={`showcard/104`} size={328} />}
+              element={<QR />}
             />
           </Route>
         </Routes>
