777f89da17dcae160ad0e21b56e40b9513dd23a9
yuhaoyann <yuhaoyann@gmail.com>
2022-02-20 22:21:42 -0500

    import usestate from react

--- src/components/pages/CardShow.js
+++ src/components/pages/CardShow.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import CardShowCard from './CardShowCard';
